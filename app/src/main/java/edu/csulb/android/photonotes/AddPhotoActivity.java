e2577f5e0b1f139003c8a479c9d1c1d652882aec
manikhanuja <mani.khanuja@pb.com>
2017-03-05 00:11:03 -0800

    Added List Activity and Add Photo Activity

---
+++ app/src/main/java/edu/csulb/android/photonotes/AddPhotoActivity.java
@@ -0,0 +1,32 @@
+package edu.csulb.android.photonotes;
+
+import android.content.Intent;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class AddPhotoActivity extends AppCompatActivity {
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_photo);
+        Button clickPhoto = (Button) findViewById(R.id.clickPictureButton);
+        clickPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                dispatchTakePictureIntent();
+            }
+        });
+    }
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+}
---
+++ app/src/main/java/edu/csulb/android/photonotes/ListActivity.java
@@ -0,0 +1,146 @@
+package edu.csulb.android.photonotes;
+
+import android.Manifest;
+import android.app.Dialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.DataSetObserver;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class ListActivity extends AppCompatActivity {
+    ArrayList<String> notes = new ArrayList<String>();
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final int REQUEST_CAMERA_PERMISSION = 1;
+    private ImageView mImageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        int permissionCheck = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA);
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.CAMERA},
+                    REQUEST_CAMERA_PERMISSION);
+        }
+
+        //Add notes
+        notes.add("Test1");
+        notes.add("Test2");
+        notes.add("Test3");
+        notes.add("Test4");
+        notes.add("Test5");
+        //Add List Adapter for Notes Application
+        ListView listViewNotes = (ListView) findViewById(R.id.listView);
+        listViewNotes.setAdapter(new NotesAdapter());
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent addPhoto = new Intent(getApplicationContext(), AddPhotoActivity.class);
+                startActivity(addPhoto);
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_CAMERA_PERMISSION) {
+            if (grantResults.length != 1 || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                Snackbar.make(findViewById(R.id.content_list), "Camera Permission Required", Snackbar.LENGTH_LONG)
+                  .setAction("Action", null).show();
+            }
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_list, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            mImageView.setImageBitmap(imageBitmap);
+        }
+    }
+
+
+    private class NotesAdapter extends BaseAdapter {
+    @Override
+    public int getCount() {
+        return notes.size();
+    }
+
+    @Override
+    public String getItem(int position) {
+        return notes.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return notes.get(position).hashCode();
+    }
+    @Override
+    public View getView(int position, View convertView, ViewGroup container) {
+        if (convertView == null) {
+            convertView = getLayoutInflater().inflate(R.layout.content_list, container, false);
+        }
+
+        ((TextView) convertView.findViewById(android.R.id.text1))
+                .setText(getItem(position));
+        return convertView;
+    }
+
+
+}
+}
